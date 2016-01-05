6e38eebba041c5b2d3751612ed707eaa5842fbb0
Carlo Nicolò <carlo.nicolo@studenti.unitn.it>
2016-01-05 14:47:23 +0100

    file with the queries for populating the database and comment file in the file persistence

---
+++ query.sql
@@ -0,0 +1,4 @@
+INSERT INTO `Person`(`idPerson`,`birthdate`,`email`,`firstname`,`fiscalcode`,`gender`,`lastname`,`idDoctor`) 
+VALUES ('1','273535200000',NULL,'Bob',NULL,'M','Smith',NULL),('2','273535200000',NULL,'Lucio',NULL,'M','Dalla',NULL),('3','273535200000',NULL,'Kate',NULL,'F','Moss',NULL),
+('4','273535200000',NULL,'Lenny',NULL,'M','Kravitz',NULL),
+('5','273535200000',NULL,'Yara',NULL,'F','Luviza',NULL);
